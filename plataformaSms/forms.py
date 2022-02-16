49526320979ec9385928a456f164ce3f32210fd9
Romilson Lemes <romilsonlemes@gmail.com>
2022-02-16 02:20:23 -0300

    copiar a pasta plataformaSms
    Com este Commit, toda pasta do Projeto Option smsMobileMarket estará estabilizada.
    
    close #7

---
+++ plataformaSms/forms.py
@@ -0,0 +1,97 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, IntegerField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from plataformaSms.models import Usuario
+from flask_login import current_user
+
+# A classe abaixo é utilizada para controlar os campos que terá no formulário
+class FormCriarConta(FlaskForm):
+    username                = StringField('Nome de usuário', validators=[DataRequired()])
+    email                   = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha                   = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
+    confirmacao_senha       = PasswordField('Confirmação da Senha', validators=[DataRequired(), EqualTo('senha')])
+    botao_submit_CriarConta = SubmitField('Criar Conta')
+
+    def validate_email(self, email):
+        usuario = Usuario.query.filter_by(email=email.data).first()
+        if usuario:
+            raise ValidationError(f'O e-mail acima do usuário {usuario.username}  já está cadastrado. Cadastre-se com outro e-mail ou faça login para continuar')
+
+# A classe dos campos do Formulário de Login
+class FormLogin(FlaskForm):
+    email           = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha           = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
+    lembrar_dados   =   BooleanField('Lembrar dados e acesso')
+    botao_submit_Login = SubmitField('Fazer Login')
+
+
+# A classe dos campos de edição do Formulário do Perfil do Usuário
+class FormEditarPerfil(FlaskForm):
+    username        = StringField('Nome de Usuário', validators=[DataRequired()])
+    email           = StringField('E-mail', validators=[DataRequired(), Email()])
+    foto_perfil = FileField('Atualizar Foto de Perfil', validators=[FileAllowed(['jpg', 'jpeg', 'png'])])
+
+    # ---------------------------------------------------
+    # ------         Cursos da Hash TAG            ------
+    # ---------------------------------------------------
+    curso_Excel     = BooleanField('Excel Impressionador')
+    curso_Vba       = BooleanField('VBA Impressionador')
+    curso_Python    = BooleanField('Python Impressionador')
+    curso_PowerBi   = BooleanField('Power BI Impressionador')
+    curso_ppt       = BooleanField('Apresentações Impressionadoras')
+    curso_Sql       = BooleanField('SQL Impressionador')
+    # ---------------------------------------------------
+    botao_submit_EditarPerfil = SubmitField('Confirmar Edição')
+
+    def validate_email(self, email):
+        if current_user.email != email.data:
+            usuario = Usuario.query.filter_by(email=email.data).first()
+            if usuario:
+                raise ValidationError(f'Já existe o usuário com esse e-mail. Cadastre outro e-mail')
+
+# A classe dos campos para criação / Edição de Posts de Usuários
+class FormCriarPost(FlaskForm):
+    titulo          = StringField('Titulo do Post', validators=[DataRequired(), Length(2, 140)])
+    corpo           = TextAreaField('Escreva o seu Post Aqui', validators=[DataRequired()])
+    botao_submit    = SubmitField('Criar Post')
+
+""""---------------------------------------
+----- Novos Formularios para a Optin  ----- 
+-------------------------------------------
+"""
+class FormCadastroModulos(FlaskForm):
+    descrModule = StringField('Informar a descrição do Módulo', validators=[DataRequired(), Length(3, 20)])
+    fixed_ip = StringField('Informar o IP do Módulo', validators=[DataRequired(), Length(7, 15)])
+    udp_Port = IntegerField('Informar a Porta UDP', validators=[DataRequired(), Length(2, 4)])
+    activeModule = BooleanField('Modulo Ativo ?')
+    botao_Salvar = SubmitField('Salvar')
+# ---------------------------------------
+    def validate_module(self, email):
+        pass
+        """"
+        cadModule = CadModules.query.filter_by(descr_module=descr_module.data).first()
+        if cadModule:
+            raise ValidationError(f'Este módulo já esta cadastrado com esta descrição')
+        """""
+
+
+""""-----------------------------------------
+----- CADASTRO DE CONFIGURAÇÕES DE XMLs ----- 
+---------------------------------------------
+"""
+class FormCadConfiguraXmls(FlaskForm):
+    # Configurar o Formato XMLS que vai mandar para o Equipamento
+    descrModule = StringField('Informar a descrição do Módulo', validators=[DataRequired(), Length(3, 20)])
+    fixed_ip = StringField('Informar o IP do Módulo', validators=[DataRequired(), Length(7, 15)])
+    udp_Port = IntegerField('Informar a Porta UDP', validators=[DataRequired(), Length(2, 4)])
+    activeModule = BooleanField('Modulo Ativo ?')
+    botao_Salvar = SubmitField('Salvar')
+# ---------------------------------------
+    def validate_cadconfxmls(self, email):
+        pass
+        """"
+        cadModule = CadModules.query.filter_by(descr_module=descr_module.data).first()
+        if cadModule:
+            raise ValidationError(f'Este módulo já esta cadastrado com esta descrição')
+        """""
---
+++ plataformaSms/routes.py
@@ -0,0 +1,207 @@
+import flask_bcrypt
+from flask import render_template, redirect, url_for, flash, request, abort
+from plataformaSms import app, database, bcrypt
+from plataformaSms.forms import FormLogin, FormCriarConta, FormEditarPerfil, FormCadastroModulos,\
+     FormCadConfiguraXmls, \
+     FormCriarPost
+from plataformaSms.models import Usuario, Post, CadModules
+from flask_login import login_user, logout_user, current_user, login_required
+import secrets
+import os
+from PIL import Image  # Biblioteca para compactar Imagem
+
+
+# Configuração Inicial
+@app.route('/')
+def home():
+    posts = Post.query.order_by(Post.id.desc()) # ordernar os registros pela Ordem Decrente
+    return render_template('home.html', posts=posts)
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+
+@app.route('/usuarios')
+@login_required
+def usuarios():
+    listaUsuarios = Usuario.query.all()
+    return render_template('usuarios.html', listaUsuarios=listaUsuarios)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form_login = FormLogin()
+    form_criarconta = FormCriarConta()
+    if form_login.validate_on_submit()  and 'botao_submit_Login' in request.form:
+        #Verificar se o usuário existe
+        usuario = Usuario.query.filter_by(email=form_login.email.data).first()
+
+        if usuario and bcrypt.check_password_hash(usuario.senha, form_login.senha.data):
+            login_user(usuario, remember=form_login.lembrar_dados.data)
+            # Exibir msg de Login Bem Sucedido
+            print(f"Login Realizado com sucesso pelo email: {form_login.email.data}")
+            flash(f"Login feito com sucesso no email: {form_login.email.data}", "alert-success")
+            param_next = request.args.get('next')
+            if param_next:
+                return redirect(param_next)
+            else:
+                return redirect(url_for('home'))
+        else:
+            flash(f"Falha no Login: {form_login.email.data} ou Senha errada !!", "alert-danger")
+
+
+    if form_criarconta.validate_on_submit() and 'botao_submit_CriarConta' in request.form:
+        # Criar o Usuario
+        # Critografar a senha do Usuário antes de Gravar na base de dados
+        senha_criptog = bcrypt.generate_password_hash(form_criarconta.senha.data)
+        usuario = Usuario(username=form_criarconta.username.data,
+                          email=form_criarconta.email.data,
+                          senha=senha_criptog)
+
+        # Adicionar a Sessão e commitar o Registro
+        database.session.add(usuario)
+        database.session.commit()
+
+        print(f"Criada a conta do email: {form_login.email.data} com sucesso")
+        flash(f"Conta criada para o email: {form_login.email.data}", "alert-success")
+        return redirect(url_for('home'))
+
+    return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
+
+
+@app.route('/sair')
+@login_required
+def sair():
+    logout_user()
+    flash(f'Logout feito com Sucesso', 'alert-success')
+    return redirect(url_for('home'))
+
+
+@app.route('/perfil')
+@login_required
+def perfil():
+    foto_perfil = url_for('static', filename='fotos_perfil/{}'.format(current_user.foto_perfil))
+    return render_template('perfil.html', foto_perfil=foto_perfil)
+
+
+def salvar_imagem(imagem):
+    # Adicionar um código aleatorio no nome da imagem
+    codigo = secrets.token_hex(8)
+    nome, extensao = os.path.splitext(imagem.filename)
+    nome_arquivo_foto = nome + codigo + extensao
+    print (f'Nome Arquivo de Foto {nome_arquivo_foto}')
+    caminho_completo = os.path.join(app.root_path, 'static/fotos_perfil', nome_arquivo_foto)
+    # Reduzir o tamanho da Imagem
+    tamanho = (200,200)
+    imagem_reduzida = Image.open(imagem)
+    imagem_reduzida.thumbnail(tamanho)
+    # Salvar a imagem
+    imagem_reduzida.save(caminho_completo)
+    # Mudar o campo foto_perfil do usuário para o novo nome da imagem
+    return nome_arquivo_foto
+
+
+def atualizar_cursos(form):
+    lista_cursos = []
+    for campo in form:
+        if 'curso_' in campo.name:
+            if campo.data: # Verifica se o combo do curso foi marcado
+                lista_cursos.append(campo.label.text)
+    return ';'.join(lista_cursos) # Retorna a lista de Cursos separados por ;
+
+
+@app.route('/perfil/editar', methods=['GET', 'POST'])
+@login_required
+def editar_perfil():
+    form = FormEditarPerfil()
+    if form.validate_on_submit():
+        current_user.email = form.email.data
+        current_user.username = form.username.data
+        # Configurar a validação Foto Selecionada para gravar
+        if form.foto_perfil.data:
+            nome_imagem = salvar_imagem(form.foto_perfil.data)
+            current_user.foto_perfil = nome_imagem
+        # Salvar os Cursos Escolhidos pelo Usuário
+        current_user.cursos = atualizar_cursos(form)
+        print (current_user.cursos)
+        database.session.commit()
+        flash('Perfil atualizado com Sucesso', 'alert-success')
+        return redirect(url_for('perfil'))
+    elif request.method == "GET":
+        form.email.data     = current_user.email
+        form.username.data  = current_user.username
+
+    foto_perfil = url_for('static', filename='fotos_perfil/{}'.format(current_user.foto_perfil))
+    return render_template('editarperfil.html', foto_perfil=foto_perfil, form=form)
+
+
+@app.route('/post/criar', methods=['GET', 'POST'])
+@login_required
+def criar_post():
+    form = FormCriarPost()
+    if form.validate_on_submit():
+        post  = Post(titulo=form.titulo.data, corpo=form.corpo.data, autor=current_user)
+
+        # Adicionar a Sessão e commitar o Registro
+        database.session.add(post)
+        database.session.commit()
+
+        print(f"Criado o Post com o título: {form.titulo.data} com sucesso")
+        flash(f"Criado o Post com o título: {form.corpo.data}", "alert-success")
+        return redirect(url_for('home'))
+    return render_template('criarpost.html', form=form)
+
+@app.route('/post/<post_id>', methods=['GET', 'POST'])
+@login_required
+def exibir_post(post_id):
+    post = Post.query.get(post_id)
+    # Verificar se o Usuário que esta chamando se ele é o dono do Post
+    if current_user == post.autor:
+        form = FormCriarPost()
+        if request.method == 'GET':
+            form.titulo.data = post.titulo
+            form.corpo.data = post.corpo
+        elif form.validate_on_submit():
+            # Atualizar os dados do Post do dono do Post
+            post.titulo = form.titulo.data
+            post.corpo = form.corpo.data
+            database.session.commit()
+            flash('Post atualizado com Sucesso', 'alert-success')
+            return redirect(url_for('home'))
+    else:
+        form = None
+    return render_template('post.html', post=post, form=form)
+
+
+@app.route('/post/<post_id>/excluir', methods=['GET', 'POST'])
+@login_required
+def excluir_post(post_id):
+    post = Post.query.get(post_id)
+    if current_user == post.autor:
+        database.session.delete(post)
+        database.session.commit()
+        flash('Post Excluído com Sucesso', 'alert-danger')
+        return redirect(url_for('home'))
+    else:
+        abort(403)
+
+"""
+Páginas referentes o Sistema de Envio de Mensagens
+"""
+@app.route('/Modules', methods=['GET', 'POST'])
+@login_required
+def cad_Modules():
+    # cadModule = cadmodules.query.all()
+    form_cadastroModulos = FormCadastroModulos()
+    return render_template('cadModules.html', form_cadastroModulos=form_cadastroModulos)
+
+
+"""
+Páginas referentes o Sistema de Envio de Mensagens
+"""
+@app.route('/cadconfigxml', methods=['GET', 'POST'])
+@login_required
+def cad_conf_xmls():
+    # cadModule = cadmodules.query.all()
+    form_cad_conf_xmls = FormCadConfiguraXmls()
+    return render_template('configXML.html', form_cad_conf_xmls=form_cad_conf_xmls)
+
